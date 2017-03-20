2c51589c1567aee9d0bd9e842f3d260bfb925138
Ondrej Pisl <pisl.ondrej@gmail.com>
2017-03-20 21:05:56 +0100

    upraveni articlu, nova db a uvedeni na hosting

--- PHP/pripojeni.php
+++ PHP/pripojeni.php
@@ -1,6 +1,6 @@
 <?php
 
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "";
 $db = "fitness";
@@ -11,5 +11,26 @@ mysqli_set_charset($connect, "utf8");
 if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+  */
+$servername = "wm117.wedos.net";
+$username = "a142225_martin";
+$password = "Lamalama1+";
+$db = "d142225_martin";
+// Create connection
+$connect = mysqli_connect($servername, $username, $password, $db);
+mysqli_set_charset($connect, "utf8");
+// Check connection
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+  foreach ($_GET as $index => $hodnota){
+    $_GET[$index]=mysqli_real_escape_string($connect, $_GET[$index]);
+    //echo "GET - $index<br>";
+  }
+  foreach ($_POST as $index => $hodnota){
+    $_POST[$index]=mysqli_real_escape_string($connect, $_POST[$index]);
+    //echo "POST - $index<br>";
+  }
 //echo "Connected successfully";
 ?>
--- databaze/fitness.sql
+++ databaze/fitness.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Stř 15. bře 2017, 19:05
+-- Vytvořeno: Pon 20. bře 2017, 19:50
 -- Verze serveru: 5.7.14
 -- Verze PHP: 5.6.25
 
@@ -66,12 +66,11 @@ CREATE TABLE `uzivatele` (
 --
 
 INSERT INTO `uzivatele` (`id`, `jmeno`, `prijmeni`, `datum`, `vyska`, `email`, `heslo`, `tel`, `pohlavi`, `narodnost_id`, `info`, `prava`, `potvrzeni_pristupu`) VALUES
-(1, 'Ondřej', 'Pišl', '1997-06-26', 1.72, 'pisl.ondrej@gmail.com', '202cb962ac59075b964b07152d234b70', '775122082', 1, 1, 'ahoj, jsem ajťák!', 1, 1),
-(2, 'Pája', 'Puďas', '1997-01-01', 0, 'pavel.pudil@gmail.com', '159', '789456123', 1, 1, 'Jmenuju se pavel', 0, 1),
-(3, 'Petr', 'Pisl', '2011-11-11', 0, 'petrpisl@gmail.com', '140f6969d5213fd0ece03148e62e461e', '+420777471259', 1, 1, 'Jem otec Ond5eje Pi3la a tot8lne m', 0, 1),
-(5, 'Ondy', 'Pisl', '2011-11-11', 0, 'ondy@gmail.com', '202cb962ac59075b964b07152d234b70', '777 777 777', 1, 2, 'asdasd', 0, 1),
-(9, 'kolo', 'obecne', '1111-11-11', 0, 'koloobecne', '202cb962ac59075b964b07152d234b70', '7777777777', 1, 1, 'asdasd', 0, 0),
-(10, 'asdasd', 'asdasda', '1111-11-11', 0, 'dfdfg', 'd58e3582afa99040e27b92b13c8f2280', 'dfgdfg', 1, 1, 'dfgdfg', 0, 0);
+(1, 'admin', 'admin', '1111-11-11', 1.72, 'admin@admin.com', '21232f297a57a5a743894a0e4a801fc3', '777777777', 1, 1, 'Jsem admin.', 1, 1),
+(2, 'user', 'user', '1111-11-11', 1.9, 'user@user.com', 'ee11cbb19052e40b07aac0ca060c23ee', '666666666', 2, 2, 'Jsem user.', 0, 1),
+(3, 'Nuser', 'Nuser', '1111-11-11', 1.65, 'nuser@nuser.com', 'f30863ba7ee94163fa00e6a54a86b5aa', '555555555', 2, 1, 'Nejsem přijatý uživatel.', 0, 0),
+(11, 'Suser', 'Suser', '1997-02-01', 1.82, 'suser@suser.com', '515483996667424e5eae87559fb6f67a', '444 444 444', 1, 2, 'Smazání uživatele.', 0, 0),
+(12, 'Puser', 'Puser', '1987-12-04', 1.55, 'puser@puser.com', '8cb1ecc05c0b32c2235046b0d9f23bea', '333 333 333', 1, 1, 'Přijetí uživatele.', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -99,7 +98,34 @@ INSERT INTO `vahy` (`uzivatele_id`, `vaha`, `datum`) VALUES
 (1, 72.5, '2017-03-06'),
 (1, 82.6, '2016-12-12'),
 (1, 74, '2017-01-12'),
-(1, 77, '2017-03-12');
+(1, 77, '2017-03-12'),
+(2, 90, '2017-03-18'),
+(2, 95, '2017-03-10'),
+(2, 85, '2017-03-03'),
+(2, 87, '2017-03-01'),
+(2, 89, '2017-02-26'),
+(2, 90, '2017-02-23'),
+(2, 102, '2017-02-03'),
+(2, 99, '2017-02-10'),
+(2, 87, '2017-02-16'),
+(2, 90, '2016-12-15'),
+(2, 95, '2016-11-15'),
+(2, 88, '2016-10-15'),
+(2, 88, '2015-06-15'),
+(2, 95, '2014-06-15'),
+(2, 90, '2013-06-15'),
+(2, 82, '2012-06-15'),
+(2, 79, '2011-06-15'),
+(2, 81, '2010-06-15'),
+(2, 81, '2016-11-15'),
+(2, 85, '2016-10-15'),
+(2, 87, '2016-09-15'),
+(2, 90, '2016-08-15'),
+(2, 83, '2016-07-15'),
+(2, 79, '2016-06-15'),
+(2, 80, '2016-05-15'),
+(2, 76, '2016-04-15'),
+(2, 92, '2017-03-19');
 
 --
 -- Klíče pro exportované tabulky
@@ -130,7 +156,7 @@ ALTER TABLE `narodnosti`
 -- AUTO_INCREMENT pro tabulku `uzivatele`
 --
 ALTER TABLE `uzivatele`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
