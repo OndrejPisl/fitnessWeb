7a4c4f878b0b7b1339744ce9f305bcf990463620
Ondrej Pisl <pisl.ondrej@gmail.com>
2017-04-05 16:59:23 +0200

    novější db

--- databaze/fitness.sql
+++ databaze/fitness.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: 127.0.0.1
--- Vytvořeno: Pon 20. bře 2017, 19:50
+-- Vytvořeno: Úte 04. dub 2017, 04:28
 -- Verze serveru: 5.7.14
 -- Verze PHP: 5.6.25
 
@@ -70,7 +70,8 @@ INSERT INTO `uzivatele` (`id`, `jmeno`, `prijmeni`, `datum`, `vyska`, `email`, `
 (2, 'user', 'user', '1111-11-11', 1.9, 'user@user.com', 'ee11cbb19052e40b07aac0ca060c23ee', '666666666', 2, 2, 'Jsem user.', 0, 1),
 (3, 'Nuser', 'Nuser', '1111-11-11', 1.65, 'nuser@nuser.com', 'f30863ba7ee94163fa00e6a54a86b5aa', '555555555', 2, 1, 'Nejsem přijatý uživatel.', 0, 0),
 (11, 'Suser', 'Suser', '1997-02-01', 1.82, 'suser@suser.com', '515483996667424e5eae87559fb6f67a', '444 444 444', 1, 2, 'Smazání uživatele.', 0, 0),
-(12, 'Puser', 'Puser', '1987-12-04', 1.55, 'puser@puser.com', '8cb1ecc05c0b32c2235046b0d9f23bea', '333 333 333', 1, 1, 'Přijetí uživatele.', 0, 0);
+(12, 'Puser', 'Puser', '1987-12-04', 1.55, 'puser@puser.com', '8cb1ecc05c0b32c2235046b0d9f23bea', '333 333 333', 1, 1, 'Přijetí uživatele.', 0, 0),
+(13, 'Ondřej', 'Pišl', '1997-06-26', 1.78, 'pisl.ondrej@gmail.com', '202cb962ac59075b964b07152d234b70', '777777777', 1, 1, ' ahojo', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -82,7 +83,7 @@ CREATE TABLE `vahy` (
   `uzivatele_id` int(10) NOT NULL,
   `vaha` float NOT NULL,
   `datum` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
 
 --
 -- Vypisuji data pro tabulku `vahy`
@@ -141,7 +142,14 @@ ALTER TABLE `narodnosti`
 -- Klíče pro tabulku `uzivatele`
 --
 ALTER TABLE `uzivatele`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `narodnost_id` (`narodnost_id`);
+
+--
+-- Klíče pro tabulku `vahy`
+--
+ALTER TABLE `vahy`
+  ADD KEY `uzivatele_id` (`uzivatele_id`);
 
 --
 -- AUTO_INCREMENT pro tabulky
@@ -157,6 +165,22 @@ ALTER TABLE `narodnosti`
 --
 ALTER TABLE `uzivatele`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- Omezení pro exportované tabulky
+--
+
+--
+-- Omezení pro tabulku `narodnosti`
+--
+ALTER TABLE `narodnosti`
+  ADD CONSTRAINT `narodnosti_ibfk_1` FOREIGN KEY (`id`) REFERENCES `uzivatele` (`narodnost_id`);
+
+--
+-- Omezení pro tabulku `vahy`
+--
+ALTER TABLE `vahy`
+  ADD CONSTRAINT `vahy_ibfk_1` FOREIGN KEY (`uzivatele_id`) REFERENCES `uzivatele` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
