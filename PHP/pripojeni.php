13826ed1fea811fa4cf0e883d86d80d6db17b89c
Ondrej Pisl <pisl.ondrej@gmail.com>
2017-04-06 23:25:18 +0200

    upraveni pripojeni.php (odstraneni serveru hosting) a pridani sql injection pro local host

--- PHP/pripojeni.php
+++ PHP/pripojeni.php
@@ -11,20 +11,7 @@ mysqli_set_charset($connect, "utf8");
 if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
-  /*
-$servername = "wm117.wedos.net";
-$username = "a142225_martin";
-$password = "Lamalama1+";
-$db = "d142225_martin";
-// Create connection
-$connect = mysqli_connect($servername, $username, $password, $db);
-mysqli_set_charset($connect, "utf8");
-// Check connection
-if (!$connect) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-  foreach ($_GET as $index => $hodnota){
+foreach ($_GET as $index => $hodnota){
     $_GET[$index]=mysqli_real_escape_string($connect, $_GET[$index]);
     //echo "GET - $index<br>";
   }
@@ -32,7 +19,4 @@ if (!$connect) {
     $_POST[$index]=mysqli_real_escape_string($connect, $_POST[$index]);
     //echo "POST - $index<br>";
   }
-//echo "Connected successfully";
-   */ 
-   
 ?>
